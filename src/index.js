f6c9b0277515cdcd432700127a3f6e19c909bc87
anand_kumar <61147494+anandiiitbh@users.noreply.github.com>
2021-08-06 10:23:22 +0530

    d

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ const PORT = process.env.PORT || 3000;
 
 http
   .createServer((req, res) => {
+  // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
     if (req.method === "POST") {
       let data = "";
       req.on("data", (chunk) => {
